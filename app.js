a0bba7d5deebc3c7ec740c47fdcb61b5e4091726
MarshesDuck <mail@megzhang.com>
2021-04-11 00:01:50 -0400

    reformatted code
    cleaned up indents

--- app.js
+++ app.js
@@ -4,41 +4,46 @@ require('dotenv').config();
 
 // Jokes
 const jokes = [
-  "What rock group has four men that don't sing? Mount Rushmore.",
-  'When I was a kid, my mother told me I could be anyone I wanted to be. Turns out, identity theft is a crime.',
-  'A guy goes to his doctor because he can see into the future. The doctor asks him,"How long have you suffered from that condition?" The guy tells him, "Since next Monday."',
-  'What do sprinters eat before a race? Nothing, they fast!',
-  'What concert costs just 45 cents? 50 Cent featuring Nickelback!',
-  "What do you call a mac 'n' cheese that gets all up in your face? Too close for comfort food!",
-  "Why couldn't the bicycle stand up by itself? It was two tired!",
-  'Did you hear about the restaurant on the moon? Great food, no atmosphere!',
-  'How many apples grow on a tree? All of them!',
-  "Did you hear the rumor about butter? Well, I'm not going to spread it!",
-  'I like telling Dad jokes. Sometimes he laughs!',
-  'To whoever stole my copy of Microsoft Office, I will find you. You have my Word!',
-  'q. How do you comfort a JavaScript bug? a. You console it',
-  'When a JavaScript date has gone bad, "Don\'t call me, I\'ll callback you. I promise!"',
-  'Dev1 saw a strange JavaScript function & asked, "What is this?". Dev2 responded, "I don\'t know. I would\'ve called you, but I was in a bind"',
-  "q. Why was the JavaScript developer sad? a. Because he didn't Node how to Express himself",
-  'q. Why did Jason cover himself with bubble wrap? a. Because he wanted to make a cross-domain JSONP request',
-  "q. Why did the CoffeeScript developer keep getting lost? a. Because he couldn't find his source without a map",
-  'q. Why did the developer go broke? a. Because he used up all his cache',
-  'q. Why did the JavaScript boxer goto the chiropractor? a. Because his backbone was angular from a knockout and required attention',
+    "What rock group has four men that don't sing? Mount Rushmore.",
+    'When I was a kid, my mother told me I could be anyone I wanted to be. Turns out, identity theft is a crime.',
+    'A guy goes to his doctor because he can see into the future. The doctor asks him,"How long have you suffered from that condition?" The guy tells him, "Since next Monday."',
+    'What do sprinters eat before a race? Nothing, they fast!',
+    'What concert costs just 45 cents? 50 Cent featuring Nickelback!',
+    "What do you call a mac 'n' cheese that gets all up in your face? Too close for comfort food!",
+    "Why couldn't the bicycle stand up by itself? It was two tired!",
+    'Did you hear about the restaurant on the moon? Great food, no atmosphere!',
+    'How many apples grow on a tree? All of them!',
+    "Did you hear the rumor about butter? Well, I'm not going to spread it!",
+    'I like telling Dad jokes. Sometimes he laughs!',
+    'To whoever stole my copy of Microsoft Office, I will find you. You have my Word!',
+    'q. How do you comfort a JavaScript bug? a. You console it',
+    'When a JavaScript date has gone bad, "Don\'t call me, I\'ll callback you. I promise!"',
+    'Dev1 saw a strange JavaScript function & asked, "What is this?". Dev2 responded, "I don\'t know. I would\'ve called you, but I was in a bind"',
+    "q. Why was the JavaScript developer sad? a. Because he didn't Node how to Express himself",
+    'q. Why did Jason cover himself with bubble wrap? a. Because he wanted to make a cross-domain JSONP request',
+    "q. Why did the CoffeeScript developer keep getting lost? a. Because he couldn't find his source without a map",
+    'q. Why did the developer go broke? a. Because he used up all his cache',
+    'q. Why did the JavaScript boxer goto the chiropractor? a. Because his backbone was angular from a knockout and required attention',
 ];
 
 client.on('ready', () => {
-  console.log('Bot is ready');
-  client.user.setActivity('More human than MarshesDuck', { type: "WATCHING" }).catch(console.error)
+    console.log('Bot is ready');
+    client.user.setActivity('More human than MarshesDuck', {type: "WATCHING"}).catch(console.error)
 
 });
 
 client.on('message', (msg) => {
-  if (msg.content === '!joke'){
-  msg.reply(jokes[Math.floor(Math.random() * jokes.length)]);}
-	
-	if (msg.content === '!hello') {msg.reply('Hi');}
-	
-	if (msg.content === '!miaolaw') {msg.channel.send('https://cdn.discordapp.com/attachments/830499580234956800/830560148388708393/1-800.png');}
+    if (msg.content === '!joke') {
+        msg.reply(jokes[Math.floor(Math.random() * jokes.length)]);
+    }
+
+    if (msg.content === '!hello') {
+        msg.reply('Hi');
+    }
+
+    if (msg.content === '!miaolaw') {
+        msg.channel.send('https://cdn.discordapp.com/attachments/830499580234956800/830560148388708393/1-800.png');
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
